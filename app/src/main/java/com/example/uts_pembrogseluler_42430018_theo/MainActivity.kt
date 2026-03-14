5f7e9a1d233683b70ee8f028be4f86809d7ba9d8
theorada <nyomantheo@gmail.com>
2026-03-15 01:59:01 +0800

    fitur: implementasi navigasi intent dan pengiriman data nama dosen

--- app/src/main/java/com/example/uts_pembrogseluler_42430018_theo/MainActivity.kt
+++ app/src/main/java/com/example/uts_pembrogseluler_42430018_theo/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.uts_pembrogseluler_42430018_theo
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etNamaDosen = findViewById<EditText>(R.id.etNamaDosen)
+        val btnMasuk = findViewById<Button>(R.id.btnMasuk)
+
+        btnMasuk.setOnClickListener {
+            val namaDosen = etNamaDosen.text.toString()
+
+            // Validasi agar tidak kosong
+            if (namaDosen.isNotEmpty()) {
+                // Modul 4: Pindah halaman dengan Intent
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("EXTRA_NAMA_DOSEN", namaDosen)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Nama Dosen tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
